b168dcb7b7ce9edecbe3e9a3f2ad8fa1d1e09db3
Yasa Zaheen <yasazaheen@gmail.com>
2024-06-06 15:59:10 +0600

    Auth pages tweaks

--- src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 const Page = () => {
-  return <SignIn />;
+  return (
+    <div className="flex w-full h-screen overflow-hidden">
+      <div className="w-2/5 h-full relative">
+        <Image
+          src={"/assets/sign-in.jpg"}
+          alt="Sign In Image"
+          fill={true}
+          className="object-cover"
+        />
+      </div>
+      <div className="w-3/5 h-full flex items-center justify-center">
+        <SignIn />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/app/(main)/agency/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 const Page = () => {
-  return <SignUp />;
+  return (
+    <div className="flex w-full h-screen overflow-hidden">
+      <div className="w-2/5 h-full relative">
+        <Image
+          src={"/assets/sign-up.jpg"}
+          alt="Sign Up Image"
+          fill={true}
+          className="object-cover"
+        />
+      </div>
+      <div className="w-3/5 h-full flex items-center justify-center">
+        <SignUp />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
